73abe571e5aef29811e9c050bc6c3cd9c324ef86
Hardik Radadiya <radadiyahardikkumar@gmail.com>
2026-08-31 18:47:36 +0530

    fix url

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://job-portal-v5l9.vercel.app",
+            "https://job-portal-pd12.onrender.com",
         ],
         credentials: true,
     })
