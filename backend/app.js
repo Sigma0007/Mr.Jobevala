5d8f8e6f3dfd905f25e674f4ff72034811b99a7c
Hardik Radadiya <radadiyahardikkumar@gmail.com>
2026-09-01 09:25:20 +0530

    fixed issue

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://job-portal-v5l9.vercel.app",
+            "https://job-portal-lake-psi.vercel.app",
         ],
         credentials: true,
     })
